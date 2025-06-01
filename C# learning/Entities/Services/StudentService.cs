b4edda648304c94bc8b1d9a1185129469404ab72
Fred055 <ceferliferid1@gmail.com>
2025-06-01 18:59:40 +0200

    Updated

---
+++ C# learning/Entities/Services/StudentService.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__learning.Entities.Services
+{
+    internal class StudentService
+    {
+        Student[] studentArray = { };
+
+        void Yarat()
+        {
+            Console.WriteLine("Pls add student name:");
+            Student student = new Student();
+            student.Name = Console.ReadLine();
+
+            Array.Resize(ref studentArray, studentArray.Length + 1);
+            studentArray[studentArray.Length - 1] = student;
+        }
+
+
+
+            void TelebeleriGoster()
+            {
+                Console.WriteLine("Student List");
+
+                foreach (var student in studentArray)
+                {
+                    Console.WriteLine(student.Name);
+                }
+            }
+        
+
+    }
+}
--- C# learning/Program.cs
+++ C# learning/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Student[] studentArray = { };
+
+Yarat();
+Yarat();
+Yarat();
+Yarat();
+Yarat();
+
+void Yarat()
+{
+    Console.WriteLine("Pls add student name:");
+    Student student = new Student();
+    student.Name = Console.ReadLine();
+
+    Array.Resize(ref studentArray, studentArray.Length + 1);
+    studentArray[studentArray.Length - 1] = student;
+
+    void TelebeleriGoster()
+    {
+        Console.WriteLine("Student List");
+
+        foreach (var student in studentArray)
+        {
+            Console.WriteLine(student.Name);
+        }
+    }
+}
+
+    class Student
+    {
+        public string Name { get; set; }
+
+    }
+
+
+   
+
